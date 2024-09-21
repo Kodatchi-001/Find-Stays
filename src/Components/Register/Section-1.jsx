32741d8bfd27b93791a7949f75b1de466a12d635
Ahmed Hariri <150302563+Kodatchi-001@users.noreply.github.com>
2024-09-21 13:08:24 +0100

    Change Size Page Register

--- src/Components/Register/Section-1.jsx
+++ src/Components/Register/Section-1.jsx
@@ -2,28 +2,18 @@ import Header from "../Header/Header";
 
 export default function Section_1() {
     return <>
-        <section className="w-full lg:h-screen lg:px-6 xl:px-20">
+        <section className="w-full lg:h-screen lg:px-2 xl:px-20">
             <Header />
             <div className="w-full h-[10vh] lg:hidden"></div>
-            <div className="w-full lg:h-[85vh] flex flex-col pt-8 px-2 lg:px-4 gap-3">
-                <div className="w-full h-[10%] flex justify-between items-center text-2xl">
+            <div className="w-full h-full lg:h-[85vh] flex flex-col pt-2 lg:pt-8 px-2 lg:px-4 gap-3">
+                <div className="w-full lg:h-[10%] flex justify-between items-center text-2xl">
                     <h1>Hotel Register</h1>
                     <h1>2 Items</h1>
                 </div>
-                <div className="w-full h-auto flex flex-wrap lg:flex-col gap-5">
-                    <div className="w-[48%] lg:w-full h-[30vh] lg:h-[35vh] flex lg:border rounded-xl flex-wrap gap-2 lg:gap-0">
-                        <div className="w-full lg:w-2/6 lg:h-full flex lg:flex-col justify-between lg:items-end flex-wrap lg:hidden">
-                            <div className="w-1/2 lg:w-full lg:h-1/5 flex lg:justify-end items-center gap-1">
-                                <i className={`bx bx-heart bx-bookmark px-3 py-2 scale-90 border border-black rounded-full text-xl`}></i>
-                                <i className={`bx bx-bookmark bx-bookmark px-3 py-2 scale-90 border border-black rounded-full text-xl`}></i>
-                            </div>
-                            <div className="flex gap-2">
-                                <button className="px-4 lg:px-5 py-2 text-sm bg-green-500 text-white rounded-lg">Modifier</button>
-                                <button className="px-4 lg:px-5 py-2 text-sm bg-red-500 text-white rounded-lg">Remove</button>
-                            </div>
-                        </div>
-                        <div className="w-full lg:w-2/6 h-full rounded-xl lg:border-radius HomePage-Background"></div>
-                        <div className="w-full lg:w-4/6 lg:h-full flex lg:pl-5 lg:pr-3 py-3 flex-wrap">
+                <div className="w-full h-full lg:h-[90%] flex flex-wrap lg:flex-nowrap lg:flex-col gap-5">
+                    <div className="sm:w-[48%] lg:w-full h-full lg:h-[35vh] flex border rounded-xl flex-wrap gap-2 lg:gap-0">
+                        <div className="w-full lg:w-2/6 h-[30vh] lg:h-full rounded-xl lg:border-radius HomePage-Background"></div>
+                        <div className="w-full lg:w-4/6 lg:h-full flex lg:pl-5 lg:pr-3 py-2 lg:py-3 flex-wrap gap-4 lg:gap-0 px-1">
                             <div className="w-full lg:w-4/6 lg:h-full flex flex-col justify-between gap-3 lg:gap-2">
                                 <div className="w-full flex flex-col gap-1">
                                     <h1 className="text-4xl">Golden Horizon Hotel</h1>
@@ -50,14 +40,14 @@ export default function Section_1() {
                                     <div className="flex gap-2">
                                         <div className="w-1/2 lg:w-auto flex flex-col gap-2">
                                             <h1 className="w-full text-lg">Check-in</h1>
-                                            <div className="flex justify-center items-center gap-2 pl-4 pr-5 py-2 rounded-lg bg-gray">
+                                            <div className="flex justify-center items-center gap-2 px-2 lg:pl-4 lg:pr-5 py-2 rounded-lg bg-gray">
                                                 <i class='bx bx-calendar text-lg'></i>
                                                 <h1>01-05-2025</h1>
                                             </div>
                                         </div>
                                         <div className="w-1/2 lg:w-auto flex flex-col gap-2">
                                             <h1 className="w-full text-lg">Check-out</h1>
-                                            <div className="flex justify-center items-center gap-2 pl-4 pr-5 py-2 rounded-lg bg-gray">
+                                            <div className="flex justify-center items-center gap-2 px-2 lg:pl-4 lg:pr-5 py-2 rounded-lg bg-gray">
                                                 <i class='bx bx-calendar text-lg'></i>
                                                 <h1>01-05-2025</h1>
                                             </div>
@@ -65,8 +55,8 @@ export default function Section_1() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="w-full lg:w-2/6 lg:h-full lg:flex lg:flex-col justify-between lg:items-end flex-wrap hidden">
-                                <div className="w-1/2 lg:w-full lg:h-1/5 flex lg:justify-end items-center gap-1">
+                            <div className="w-full lg:w-2/6 lg:h-full flex lg:flex-col justify-between lg:items-end flex-wrap">
+                                <div className="w-1/3 lg:w-full lg:h-1/5 flex lg:justify-end items-center gap-1">
                                     <i className={`bx bx-heart bx-bookmark px-3 py-2 scale-90 border border-black rounded-full text-xl`}></i>
                                     <i className={`bx bx-bookmark bx-bookmark px-3 py-2 scale-90 border border-black rounded-full text-xl`}></i>
                                 </div>
@@ -77,19 +67,9 @@ export default function Section_1() {
                             </div>
                         </div>
                     </div>
-                    <div className="w-[48%] lg:w-full h-[30vh] lg:h-[35vh] flex lg:border rounded-xl flex-wrap gap-2 lg:gap-0">
-                        <div className="w-full lg:w-2/6 lg:h-full flex lg:flex-col justify-between lg:items-end flex-wrap lg:hidden">
-                            <div className="w-1/2 lg:w-full lg:h-1/5 flex lg:justify-end items-center gap-1">
-                                <i className={`bx bx-heart bx-bookmark px-3 py-2 scale-90 border border-black rounded-full text-xl`}></i>
-                                <i className={`bx bx-bookmark bx-bookmark px-3 py-2 scale-90 border border-black rounded-full text-xl`}></i>
-                            </div>
-                            <div className="flex gap-2">
-                                <button className="px-4 lg:px-5 py-2 text-sm bg-green-500 text-white rounded-lg">Modifier</button>
-                                <button className="px-4 lg:px-5 py-2 text-sm bg-red-500 text-white rounded-lg">Remove</button>
-                            </div>
-                        </div>
-                        <div className="w-full lg:w-2/6 h-full rounded-xl lg:border-radius HomePage-Background"></div>
-                        <div className="w-full lg:w-4/6 lg:h-full flex lg:pl-5 lg:pr-3 py-3 flex-wrap">
+                    <div className="sm:w-[48%] lg:w-full h-full lg:h-[35vh] flex border rounded-xl flex-wrap gap-2 lg:gap-0">
+                        <div className="w-full lg:w-2/6 h-[30vh] lg:h-full rounded-xl lg:border-radius HomePage-Background"></div>
+                        <div className="w-full lg:w-4/6 lg:h-full flex lg:pl-5 lg:pr-3 py-2 lg:py-3 flex-wrap gap-4 lg:gap-0 px-1">
                             <div className="w-full lg:w-4/6 lg:h-full flex flex-col justify-between gap-3 lg:gap-2">
                                 <div className="w-full flex flex-col gap-1">
                                     <h1 className="text-4xl">Golden Horizon Hotel</h1>
@@ -116,14 +96,14 @@ export default function Section_1() {
                                     <div className="flex gap-2">
                                         <div className="w-1/2 lg:w-auto flex flex-col gap-2">
                                             <h1 className="w-full text-lg">Check-in</h1>
-                                            <div className="flex justify-center items-center gap-2 pl-4 pr-5 py-2 rounded-lg bg-gray">
+                                            <div className="flex justify-center items-center gap-2 px-2 lg:pl-4 lg:pr-5 py-2 rounded-lg bg-gray">
                                                 <i class='bx bx-calendar text-lg'></i>
                                                 <h1>01-05-2025</h1>
                                             </div>
                                         </div>
                                         <div className="w-1/2 lg:w-auto flex flex-col gap-2">
                                             <h1 className="w-full text-lg">Check-out</h1>
-                                            <div className="flex justify-center items-center gap-2 pl-4 pr-5 py-2 rounded-lg bg-gray">
+                                            <div className="flex justify-center items-center gap-2 px-2 lg:pl-4 lg:pr-5 py-2 rounded-lg bg-gray">
                                                 <i class='bx bx-calendar text-lg'></i>
                                                 <h1>01-05-2025</h1>
                                             </div>
@@ -131,8 +111,8 @@ export default function Section_1() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="w-full lg:w-2/6 lg:h-full lg:flex lg:flex-col justify-between lg:items-end flex-wrap hidden">
-                                <div className="w-1/2 lg:w-full lg:h-1/5 flex lg:justify-end items-center gap-1">
+                            <div className="w-full lg:w-2/6 lg:h-full flex lg:flex-col justify-between lg:items-end flex-wrap">
+                                <div className="w-1/3 lg:w-full lg:h-1/5 flex lg:justify-end items-center gap-1">
                                     <i className={`bx bx-heart bx-bookmark px-3 py-2 scale-90 border border-black rounded-full text-xl`}></i>
                                     <i className={`bx bx-bookmark bx-bookmark px-3 py-2 scale-90 border border-black rounded-full text-xl`}></i>
                                 </div>
