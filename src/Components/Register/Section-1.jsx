49ec34dd8075cb19a6562bc92053a84f57b737ed
Ahmed Hariri <150302563+Kodatchi-001@users.noreply.github.com>
2024-09-26 22:20:45 +0100

    Create New Logic Page Booking

--- src/Components/Register/Section-1.jsx
+++ src/Components/Register/Section-1.jsx
@@ -1,38 +1,64 @@
 import { useEffect, useState } from "react";
 import Header from "../Header/Header";
-
 export default function Section_1() {
-    const [userReservations, setUserReservations] = useState([]);
     const [userName, setuserName] = useState('');
-
+    const [userReservations, setUserReservations] = useState([]);
+    /*-------------------------------------------------*/
     const users = JSON.parse(localStorage.getItem('users')) || [];
     const currentUserEmail = localStorage.getItem('currentUserEmail');
     const userIndex = users.findIndex(user => user.userEmail == currentUserEmail);
+    const [LikeHotels, setLikeHotels] = useState([]);
+    const [SavedHotels, setSavedHotels] = useState([]);
+    /*-------------------------------------------------*/
+    const LikeHotel = index => {
+        if (userIndex !== -1) {
+            if (users[userIndex].Like.includes(index)) {
+                users[userIndex].Like = users[userIndex].Like.filter(item => item !== index)
+            } else {
+                users[userIndex].Like.push(index)
+            }
+            localStorage.setItem('users', JSON.stringify(users));
+            setLikeHotels(users[userIndex].Like)
+        }
+    }
+    const SaveHotel = index => {
+        if (userIndex !== -1) {
+            if (users[userIndex].Save.includes(index)) {
+                users[userIndex].Save = users[userIndex].Save.filter(item => item !== index)
+            } else {
+                users[userIndex].Save.push(index)
+            }
+            localStorage.setItem('users', JSON.stringify(users));
+            setSavedHotels(users[userIndex].Save);
+        }
+    }
+    const SuprimmeHotel = index => {
+        const reservations = users[userIndex].reservations.filter(items => items.id !== index)
+        setUserReservations(reservations);
+    }
+    /*-------------------------------------------------*/
     useEffect(() => {
         if (userIndex !== -1) {
-            setUserReservations(users[userIndex].reservations);
             setuserName(users[userIndex].fullName)
+            setLikeHotels(users[userIndex].Like || []);
+            setSavedHotels(users[userIndex].Save || []);
+            setUserReservations(users[userIndex].reservations);
         }
-    }, []);
+    }, [userIndex]);
     return <>
         <section className="w-full lh-screen lg:px-2 xl:px-20">
             <Header />
             <div className="w-full h-[10vh] lg:hidden"></div>
-            <div className="w-full h-full flex flex-col pt-4 lg:pt-8 px-2 lg:px-4 gap-5">
-                <div className="w-full lg:h-[10%] flex justify-between items-center">
+            <div className="w-full h-full flex flex-col pt-4 lg:pt-8 lg:px-4 gap-5">
+                <div className="w-full lg:h-[10%] flex flex-col lg:flex-row justify-between items-center gap-5 lg:gap-0">
                     <div>
                         <h1 className={`text-2xl ${userName.trim() == '' ? 'flex' : 'hidden'}`}>Go! Create an Account to Register</h1>
-                        <h1 className={`text-2xl ${userName.trim() == '' ? 'hidden' : 'flex'}`}>Hello, {userName}!</h1>
+                        <h1 className={`text-2xl ${userName.trim() == '' ? 'hidden' : 'flex'}`}>Hello, {userName} !</h1>
                     </div>
-                    {/* <h1>{userReservations.length} Items</h1> */}
-                    <select className="rounded-lg px-2 text-xl py-2 border">
-                        <option value="Register" defaultChecked>Register</option>
-                        <option value="Like">Like</option>
-                        <option value="Save">Save</option>
-                    </select>
+                    <h1 className="text-2xl">{users[userIndex].reservations.length} Items</h1>
                 </div>
                 <div className="w-full h-full lg:h-[90%] flex flex-wrap lg:flex-nowrap lg:flex-col gap-5">
-                    {userReservations.length > 0 ? userReservations.map((items) => (
+                    {userReservations.map((items) => (
                         <div key={items.id} className="sm:w-full h-full lg:h-[28vh] flex rounded-lg flex-wrap p-5 px-4 gap-2 lg:gap-0 Shadow">
                             <div className="w-full lg:w-1/2 lg:h-full flex flex-col justify-between gap-5 lg:gap-0">
                                 <div>
@@ -55,9 +81,12 @@ export default function Section_1() {
                             </div>
                             <div className="w-full lg:w-1/2 h-full flex flex-wrap-reverse lg:flex-col justify-between items-end gap-2 lg:gap-0">
                                 <div className="w-full flex justify-end">
-                                    <i className={`bx bx-heart px-3 py-2 scale-90 border border-black rounded-full text-xl cursor-pointer duration-200 hover:bg-red-500 hover:text-white hover:border-transparent`}></i>
-                                    <i className={`bx bx-bookmark px-3 py-2 scale-90 border border-black rounded-full text-xl cursor-pointer duration-200 hover:bg-green-500 hover:text-white hover:border-transparent`}></i>
-                                    <i class='bx bx-trash px-3 py-2 scale-90 border border-black rounded-full text-xl cursor-pointer duration-200 hover:bg-black hover:text-white hover:border-transparent'></i>
+                                    <i className={`bx ${LikeHotels.includes(items.id) ? 'bxs-heart text-red-500' : 'bx-heart'} px-3 py-2 scale-90 border border-black rounded-full text-xl cursor-pointer`}
+                                        onClick={() => LikeHotel(items.id)}></i>
+                                    <i className={`bx ${SavedHotels.includes(items.id) ? 'bxs-bookmark text-green-500' : 'bx-bookmark'} px-3 py-2 scale-90 border border-black rounded-full text-xl cursor-pointer`}
+                                    onClick={() => SaveHotel(items.id)}></i>
+                                    <i class='bx bx-trash px-3 py-2 scale-90 border border-black rounded-full text-xl cursor-pointer duration-200 hover:bg-black hover:text-white hover:border-transparent'
+                                        onClick={() => SuprimmeHotel(items.id)}></i>
                                 </div>
                                 <div className="w-full flex justify-between lg:justify-end gap-2">
                                     <div className="flex flex-col gap-2">
@@ -85,12 +114,7 @@ export default function Section_1() {
                                 </div>
                             </div>
                         </div>
-                    )) : (
-                        <div className="w-full h-[50vh] flex flex-col justify-center items-center gap-5">
-                            <i class='bx bx-map text-8xl'></i>
-                            <h1 className="w-1/2 lg:w-1/4 text-center text-3xl">No bookings available now</h1>
-                        </div>
-                    )}
+                    ))}
                 </div>
             </div>
         </section >
